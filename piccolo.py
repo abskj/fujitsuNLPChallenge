6146b2363bcae1eaaa50622691b01e7ac3c5e859
Abhishek Jha <j7.abhi@gmail.com>
2018-05-23 22:09:31 +0530

    dataset class created
    myDataset in piccolo.py

--- piccolo.py
+++ piccolo.py
@@ -1,45 +1,74 @@
-import torch
-from torch.utils import data
 import numpy as np
-# local data address: C:\Users\sauron\Downloads\word_embeddings_train01.csv
-
-class Dataset(data.Dataset):
-  def __init__(self,filename1):
-    self.length=0
-    self.filename=filename1
-  def __len__(self):
-    if self.length ==0:
-      with open(self.filename) as infile:
-        for line in infile:
-          self.length+=1
-      return self.length
-    else:
-      return self.length
-  def __getitem__(self,idx):
-    i=0
-    with open(self.filename) as infile:
-      for line in infile:
-        i+=1
-        if i==idx:
-          data=np.fromstring(line,sep=',')
-          data=data[:24000].reshape(80,300)
-    x=torch.from_numpy(data)
-    return x
-
-  def __getitemy__(self,idx):
-    i=0
-    with open(self.filename) as infile:
-      for line in infile:
-        i+=1
-        if i==idx:
-          data=np.fromstring(line,sep=',')
-          data=data[idx]
-    x=data
-    return x
-
-ds=Dataset(r'C:\Users\sauron\Downloads\word_embeddings_train01.csv')
-print(ds[5])
+import torch
+import spacy
+from torch.utils.data import Dataset, DataLoader
+import json
 
 
-          
-    
+class myDataset(Dataset):
+	def __init__(self,filename,y=0):
+		self.nlp=spacy.load("en_core_web_md")
+		self.filename=filename
+		self.length=0
+		with open(filename) as fp:
+			s=fp.read()
+		self.arr=s.split('\n')
+		self.y=y
+
+
+	def __len__(self):
+		if self.length == 0:
+			for i in range(len(self.arr)-1):
+				try:
+					obj=json.loads(self.arr[i])
+					self.length+=len(obj["sentences"])
+				except ValueError:
+					print('got a Value error(most probably improper JSON at '+str(i))
+					pass
+		return self.length
+	
+	def __getitem__(self,idx):
+		index=0
+		for i in range(len(self.arr)):
+			obj=json.loads(self.arr[i])
+			no_of_ans=len(obj["sentences"])
+			if((index+no_of_ans)>=idx) :
+				break
+			index+=no_of_ans
+		print(i)
+		if self.y :
+			if (idx-index) in obj["candidates"]:
+				return 1
+			else:
+				return 0
+		question=self.nlp(obj["question"])
+		answer=self.nlp(obj["sentences"][idx-index])
+		list_of_tokens=[]
+		for i in range(40):
+			if(i<len(question)):
+				list_of_tokens.append(torch.Tensor(question[i].vector).cuda(0))
+			else:
+				list_of_tokens.append(torch.zeros(300).cuda(0))
+		for i in range(40):
+			if(i<len(answer)):
+    				list_of_tokens.append(torch.Tensor(answer[i].vector).cuda(0))
+			else:
+				list_of_tokens.append(torch.zeros(300).cuda(0))
+		sentence_to_embedding=torch.stack(list_of_tokens,dim=0)
+		return sentence_to_embedding
+		
+		
+
+d=myDataset('.\datasets\selqa-evaluater\SelQA-ass-train.json',y=1)
+print(len(d))
+print(len(d))
+print(len(d))
+print(len(d))
+print(len(d))
+print(len(d))
+print(d[50000])
+print(d[2000])
+print(d[20500])
+print(d[52000])
+for i in range(200):
+	print(d[i])
